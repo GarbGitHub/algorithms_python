a7b93e097a24eedf51a5845c37f10dab6a347423
Sergey Kluikov <garbich@gmail.com>
2022-06-01 21:06:27 +0300

    edit file

--- less_1_task_1.py
+++ less_1_task_1.py
@@ -3,7 +3,7 @@
 https://drive.google.com/file/d/1LNKYIAR-qy9HLNaE2lWW3VSjccE5I75x/view?usp=sharing
 """
 
-a = int(input('введите целое натуральное трехзначное число: '))
+a = int(input('введите целое натуральное трехзначное число!: '))
 
 b = a // 100
 c = (a // 10) % 10
